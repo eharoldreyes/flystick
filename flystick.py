40cd889d4388443c85636ad9e6b0ace3ab02bda6
Harold Reyes <eharoldreyes@gmail.com>
2021-11-19 22:32:46 +0800

    Remove displays

--- flystick.py
+++ flystick.py
@@ -15,8 +15,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-from flystick_config import (
-    CHANNELS, DISPLAY, DISPLAY_BRIGHTNESS, PPM_OUTPUT_PIN)
+from flystick_config import (CHANNELS,PPM_OUTPUT_PIN)
 
 import logging
 import pygame
@@ -31,43 +30,10 @@ except ImportError as e:
     logging.warn("Failed to load pigpio library, running in debug mode")
     pigpio = None
 
-try:
-    import scrollphat
-except (ImportError, IOError) as e:
-    logging.warn(e, exc_info=True)
-    logging.warn("Failed to load Scroll pHAT library, you'll be missing all the fancy graphics")
-    scrollphat = None
-
 
 _running = False
 
 _output = ()
-
-
-def render():
-    # LED check
-    scrollphat.clear_buffer()
-    for col in range(0, 13):
-        if col > 1:
-            scrollphat.set_col(col - 2, 0)
-        if col < 11:
-            scrollphat.set_col(col, 0b11111)
-        scrollphat.update()
-        time.sleep(.1)
-
-    time.sleep(.3)
-
-    while _running:
-        scrollphat.clear_buffer()
-        # ``_output`` access should be thread-safe; de-referenced just once
-        for rend, value in zip(DISPLAY, _output):
-            try:
-                rend(value, scrollphat)
-            except ValueError as e:
-                logging.warn(e, exc_info=True)
-        scrollphat.update()
-        time.sleep(.05)
-
 
 def shutdown(signum, frame):
     global _running
@@ -96,14 +62,6 @@ def main():
         pi.wave_send_repeat(waves[-1])
     else:
         pi = None
-
-    if scrollphat:
-        scrollphat.clear()
-        scrollphat.set_brightness(DISPLAY_BRIGHTNESS)
-        # fork to avoid crash in case of I2C connection issues
-        th = threading.Thread(target=render)
-        th.daemon = True
-        th.start()
 
     prev = None
 
@@ -156,8 +114,6 @@ def main():
         # very sophisticated. (At this point, at least.)
         time.sleep(.02)
 
-    if scrollphat:
-        scrollphat.clear()
     if pi:
         pi.stop()
 
--- flystick_config.py
+++ flystick_config.py
@@ -8,9 +8,6 @@ from flystick_conf_models import *
 
 stick = Joystick(0)
 
-# aileron trim, hat side-to-side axis
-ail_trim = stick.hat_switch(hat=0, axis=0, positions=41, initial=20)
-
 # Raspberry Pi GPIO pin where to output the PPM signal.
 # Pin map: http://wiki.mchobby.be/images/3/31/RASP-PIZERO-Correspondance-GPIO.jpg
 # (Connect this pin to the RC transmitter trainer port.)
@@ -18,43 +15,19 @@ PPM_OUTPUT_PIN = 18
 
 # Output (PPM) channels.
 CHANNELS = (
-    # channel 1: aileron with trim
-    stick.axis(0) + ail_trim * 0.5,
-    # a more elaborate example with reverse, offset, weight and trim:
-    #(-stick.axis(0) + 0.1) * 0.7 + ail_trim * 0.5,
+    # channel 1: aileron
+    stick.axis(0),
     # channel 2: elevator (reversed)
     -stick.axis(1),
     # channel 3: throttle (reversed)
-    -stick.axis(2),
-    # channel 4: flight mode
-    # hat up-down axis, 5 states to match scrollphat vertical resolution
+    -stick.axis(3),
+    # channel 4: rudder (reversed)
+    stick.axis(2),
+    # channel 5: flight mode
     stick.hat_switch(hat=0, axis=1, positions=5),
-    # channels 5-8: buttons demo
+    # channels 6-8: buttons demo
     stick.button(0),
     stick.button(1),
     stick.button(2),
-    stick.button(3),
 )
 
-# dual-channel display component
-stick_dot = XYDot(col=5)
-
-# Render outputs (channels). One-to-one line match to CHANNELS.
-DISPLAY = (
-    # channel 1: dot horizontal axis
-    stick_dot.horizontal(),
-    # channel 2: dot vertical axis
-    stick_dot.vertical(),
-    # channel 3: throttle bar
-    YBar(col=0, width=2),
-    # channel 4: flight mode switch
-    YDot(col=9),
-    # channels 5-8: buttons demo
-    Block(corner=(10, 0)),
-    Block(corner=(10, 1)),
-    Block(corner=(10, 2)),
-    Block(corner=(10, 3)),
-)
-
-# TODO what's the range? 128? http://www.issi.com/WW/pdf/31FL3730.pdf
-DISPLAY_BRIGHTNESS = 10
