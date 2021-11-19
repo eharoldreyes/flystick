93d68b682411b1f9fa1d20a46e1f0de4df2a293f
Harold Reyes <eharoldreyes@gmail.com>
2021-11-19 23:02:53 +0800

    change pygametest.py

--- pygametest.py
+++ pygametest.py
@@ -4,19 +4,16 @@ pygame.display.init()
 pygame.joystick.init()
 pygame.joystick.Joystick(0).init()
 
-# Get the name of the joystick and print it
+# Prints the joystick's name
 JoyName = pygame.joystick.Joystick(0).get_name()
 print "Name of the joystick:"
 print JoyName
-
-# Get the number of axes
+# Gets the number of axes
 JoyAx = pygame.joystick.Joystick(0).get_numaxes()
 print "Number of axis:"
 print JoyAx
 
-# Print the values for the axes
-pygame.event.pump()
-print pygame.joystick.Joystick(0).get_axis(0)
-print pygame.joystick.Joystick(0).get_axis(1)
-print pygame.joystick.Joystick(0).get_axis(2)
-print pygame.joystick.Joystick(0).get_axis(3)
+# Prints the values for axis0
+while True:
+        pygame.event.pump()
+        print pygame.joystick.Joystick(0).get_axis(0)
