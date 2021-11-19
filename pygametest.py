4b29db981404a702d1c166afcfd101de5dfe1096
Harold Reyes <eharoldreyes@gmail.com>
2021-11-19 23:06:10 +0800

    change pygametest.py axes

--- pygametest.py
+++ pygametest.py
@@ -2,18 +2,21 @@ import pygame
 
 pygame.display.init()
 pygame.joystick.init()
-pygame.joystick.Joystick(0).init()
+
+
+js0 = pygame.joystick.Joystick(0)
+js0.init()
 
 # Prints the joystick's name
-JoyName = pygame.joystick.Joystick(0).get_name()
+JoyName = js0.get_name()
 print "Name of the joystick:"
 print JoyName
 # Gets the number of axes
-JoyAx = pygame.joystick.Joystick(0).get_numaxes()
+JoyAx = js0.get_numaxes()
 print "Number of axis:"
 print JoyAx
 
 # Prints the values for axis0
 while True:
         pygame.event.pump()
-        print pygame.joystick.Joystick(0).get_axis(0)
+        print "axis 0" + js0.get_axis(0) + "axis 1" + js0.get_axis(1)  + "axis 2" + js0.get_axis(2)  + "axis 3" + js0.get_axis(3) 
